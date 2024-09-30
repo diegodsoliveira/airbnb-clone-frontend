c705b0f85b8575f26d4af737c9aee03a6d02d950
Diego Oliveira <diegodsoliveira@gmail.com>
2024-09-30 17:51:00 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { ButtonModule } from 'primeng/button';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ToastModule } from 'primeng/toast';
+import { ToastService } from './layout/toast.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    FontAwesomeModule,
+    NavbarComponent,
+    FooterComponent,
+    ToastModule,
+  ],
+  providers: [MessageService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'airbnb-clone-front';
+export class AppComponent implements OnInit {
+  faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+  toastService: ToastService = inject(ToastService);
+  messageService = inject(MessageService);
+
+  isListingView: boolean = true;
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+    this.listenToastService();
+  }
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  private listenToastService(): void {
+    this.toastService.sendSub.subscribe({
+      next: (newMessage) => {
+        if (newMessage && newMessage.summary !== this.toastService.INIT_STATE) {
+          this.messageService.add(newMessage);
+        }
+      },
+    });
+  }
 }
---
+++ src/app/layout/navbar/category/category.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CategoryService } from './category.service';
+import { ICategory } from './category.model';
+
+@Component({
+  selector: 'app-category',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.scss',
+})
+export class CategoryComponent implements OnInit {
+  categoryService = inject(CategoryService);
+
+  categories: ICategory[] | undefined;
+
+  currentActivatedCategory: ICategory =
+    this.categoryService.getCategoryByDefault();
+
+  ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  private fetchCategories() {
+    this.categories = this.categoryService.getCategories();
+  }
+}
